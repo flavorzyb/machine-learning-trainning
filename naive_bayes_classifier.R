5b711a6a67660ea29dba9b698fcf15ba1bb33a79
Yanbin Zhu <haker-haker@163.com>
2018-09-03 16:59:11 +0800

    add iris.xls

--- naive_bayes_classifier.R
+++ naive_bayes_classifier.R
@@ -2,3 +2,11 @@ library(e1071)
 data(iris)
 classifier <- naiveBayes(x = iris[, 1:4], iris[, 5])
 table(predict(classifier, iris), iris[,5])
+write.table(iris, 
+            file = "iris.xls", 
+            sep = "\t",
+            eol = "\n",
+            row.names = FALSE,
+            col.names = TRUE,
+            quote = TRUE,
+            fileEncoding = "UTF-8")
